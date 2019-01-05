cbff1ed1c381d82fbb70db7a8dc5fad149dfd313
Amy Chow <csamyphew.cky@gmail.com>
2019-01-05 21:14:32 +0800

    Init project with scss, bs, router, webpack, axios in pwa boilerplate

---
+++ src/main.js
@@ -0,0 +1,27 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'es6-promise/auto'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import BootstrapVue from 'bootstrap-vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(BootstrapVue)
+axios.defaults.baseURL = process.env.API_URL
+Vue.use(VueAxios, axios)
+
+// Styles
+require('./styles/app.scss')
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  template: '<App/>',
+  components: { App }
+})
---
+++ src/pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <Hello></Hello>
+    <p>{{entryCount}}</p>
+  </div>
+</template>
+
+<script>
+import Hello from '@/components/Hello.vue'
+export default {
+  name: 'Home',
+  components: {Hello}
+}
+</script>
