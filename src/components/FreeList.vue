4fd7e1564355594dfbfb994a43311a427ca0b70b
Amy Chow <csamyphew.cky@gmail.com>
2019-01-07 19:08:21 +0800

    Implement sorting for free list

--- src/components/FreeList.vue
+++ src/components/FreeList.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="free-list container">
-    <h4 class="mb-3">免費</h4>
-    <div class="row" v-if="apps.length>0" >
+    <div class="row justify-content-between align-items-center mb-3 mr-0">
+      <h4 class="col-auto">免費</h4>
+      <!-- <b-form-select class="col-auto mb-3" v-model="sort" :options="sort_options"/> -->
+      <v-select v-model="sort" :options="sort_options" :clearable="false"></v-select>
+    </div>
+    <div class="row" v-if="sortedApps.length>0" >
       <div class="app-item col-12 col-md-6 col-lg-4 my-2" v-for="(item,index) in list" :key="'free-'+index" :id="'free-'+index"
            data-aos="fade-left" :data-aos-delay="50*(index%10)" data-aos-duration="500" data-aos-once="true" 
            :data-aos-anchor="(index<10)?'.app-list':'#free-'+(Math.floor(index/10)*10-3)" data-aos-anchor-placement="top-center">
         <div class="row align-items-center">
-          <p class="rank col-2 p-4 col-sm-1 px-sm-0 mb-0">{{item.rank}}</p>
+          <p class="rank col-2 p-4 px-sm-0 mb-0">{{item.rank}}</p>
           <div class="col-3 pl-0 px-md-0">
             <img class="icon" :src="item.icon" />
           </div>
-          <div class="col-7 col-sm-8 row">
+          <div class="col-7 row">
             <p class="name col-12 mb-1">{{item.name}}</p>
             <p class="category col-12 mb-1">{{item.category}}</p>
             <p class="author col-12 mb-0">{{item.author}}</p>
@@ -30,43 +34,97 @@
 
 <script>
 import ClipLoader from 'vue-spinner/src/ClipLoader.vue'
+import vSelect from 'vue-select'
 export default {
   name: 'FreeList',
   props: ['apps', 'keyword'],
-  components: {ClipLoader},
+  components: {ClipLoader,vSelect},
   data () {
     return {
       page: 1,
       list: [],
+      sortedApps: [],
       infiniteId: +new Date(),
+      sort: {value: 'asc-rank', label: '排位(順序)'},
+      sort_options: [
+        {value: 'asc-rank', label: '排位(順序)'},
+        {value: 'desc-rank', label: '排位(倒序)'},
+        {value: 'asc-name', label: '名字(順序)'},
+        {value: 'desc-name', label: '名字(倒序)'}
+      ]
     }
   },
   watch: {
     apps: function(newVal) { 
+      this.sortedApps = sortApps(this.sort.value, newVal.slice(0))
+    },
+    sort: function() {
+      this.sortedApps = sortApps(this.sort.value, this.apps.slice(0))
+    },
+    sortedApps: function(newVal) {
       this.list = newVal.slice(0,10)
       this.page = 1
-      this.infiniteId += 1;
+      this.infiniteId += 1
       // this.$scrollTo('.home',{offset: -80})
     }
   },
   methods: {
     infiniteHandler($state) {
       var page = this.page +1
-      // console.log('inin', (this.apps.length)>(10*page-10))
-      if((this.apps.length)>(10*page-10)){
+      if((this.sortedApps.length)>(10*page-10)){
         var start = 10*page-10
         var end = 10*page
-        if((this.apps.length)<=(10*page)){
-          end = this.apps.length
+        if((this.sortedApps.length)<=(10*page)){
+          end = this.sortedApps.length
         }
-        // console.log(page, start, end)
-        this.list.push(...this.apps.slice(start,end))
+        this.list.push(...this.sortedApps.slice(start,end))
         this.page += 1
-        $state.loaded();
+        $state.loaded()
       }else{
-        $state.complete();
+        $state.complete()
       }
     }
   }
+}
+function sortApps(type,arr){
+  switch(type){
+    case 'desc-name':
+      arr.sort((a,b)=>{
+        var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+        var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+        if (nameA > nameB) {
+          return -1;
+        }
+        if (nameA < nameB) {
+          return 1;
+        }
+        return 0;
+      })
+      break
+    case 'asc-name':
+      arr.sort((a,b)=>{
+        var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+        var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+        return 0;
+      })
+      break
+    case 'desc-rank':
+      arr.sort((a,b)=>{
+        return b.rank-a.rank
+      })
+      break
+    default:
+      arr.sort((a,b)=>{
+        return a.rank-b.rank
+      })
+      break
+  }
+  return arr
 }
 </script>
