89b0bb9eb0c0924702064be82161d226edba526a
csamyphew <csamyphew.cky@gmail.com>
2020-03-24 04:09:10 +0800

    Update server route and api domain

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const request = require('request');
+const serveStatic = require('serve-static');
+const path = require('path');
 
 const app = express();
 
@@ -43,6 +45,14 @@ app.get('/app/detail/:id', (req, res) => {
     }
   )
 });
+
+//here we are configuring dist to serve app files
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function(req, res) {
+  res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
