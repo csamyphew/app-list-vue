0d7d5469d0c533bdb2ec8f8fcce3296a8c408825
csamyphew <csamyphew.cky@gmail.com>
2020-03-24 04:12:51 +0800

    Update dist path

--- index.js
+++ index.js
@@ -47,11 +47,11 @@ app.get('/app/detail/:id', (req, res) => {
 });
 
 //here we are configuring dist to serve app files
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.use('/', serveStatic(path.join(__dirname, '/client/dist')))
 
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function(req, res) {
-  res.sendFile(path.join(__dirname, '/dist/index.html'))
+  res.sendFile(path.join(__dirname, '/client/dist/index.html'))
 })
 
 const PORT = process.env.PORT || 8000;
